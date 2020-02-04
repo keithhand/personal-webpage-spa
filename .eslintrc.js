380693f88d642116930677d142cc641a1210634c
Keith Hand <keith@hand.technology>
2020-02-03 19:42:03 -0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'airbnb'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+};
